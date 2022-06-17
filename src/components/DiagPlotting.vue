0ac7911d95d5b734d8185d08b53c4671c6443531
Paolo Lupo <paologuillenlupo@gmail.com>
2022-06-16 22:19:09 -0500

    agregando libreria de graficas

---
+++ src/components/DiagPlotting.vue
@@ -0,0 +1,16 @@
+<template>
+  <highchart :options="chartOptions"> </highchart>
+
+
+</template>
+
+<script setup>
+import {Chart as highchart} from 'highcharts-vue'
+
+let chartOptions;
+chartOptions =  {
+  series: [{
+    data: [1, 2, 3] // sample data
+  }]
+}
+</script>
