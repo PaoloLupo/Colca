8cb1b35ae8b7f5275780b0afb88018cabbe47e92
Paolo Lupo <paologuillenlupo@gmail.com>
2022-06-16 22:19:44 -0500

    modificando valores iniciales y agregando componente de plotting

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import MainBar from './components/MainBar.vue';
 import ColumnGeoProps from './components/ColumnGeoProps.vue';
+import DiagPlotting from './components/DiagPlotting.vue';
 </script>
 
 <template>
   <main-bar class="p-0"/>
   <ColumnGeoProps />
+  <DiagPlotting/>
 
 </template>
 
