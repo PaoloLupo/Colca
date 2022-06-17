7d0dbe2fe8c4bd17d8b6f4f9914369cf76567dae
Paolo Lupo <paologuillenlupo@gmail.com>
2022-06-16 20:07:01 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import MainBar from './components/MainBar.vue';
+</script>
+
+<template>
+  <main-bar class="p-0"/>
+</template>
+
---
+++ src/components/MainBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Menubar :model="items">
+      <template #start>
+        <img src="../../src-tauri/icons/Square44x44Logo.png" alt="logo" height="40" class="mx-8">
+      </template>
+    </Menubar>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+const items = ref([
+  {
+    label:'File',
+    icon:'pi pi-fw pi-file',
+  }
+])
+</script>
+
+<style scoped>
+
+</style>
